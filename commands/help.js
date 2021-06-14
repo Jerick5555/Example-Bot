7623ea54d1560ec7af18d4b3f14fd9a9a8e3ede5
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2021-06-14 11:18:38 +0800

    made example bot

---
+++ commands/help.js
@@ -0,0 +1,61 @@
+module.exports = {
+    name: "help",
+    description: "Help command",
+    syntax: "{Command to check}",
+    category: "Utility",
+    execute(message, args) {
+        const Discord = require('discord.js');
+        const client = require('../index.js').client;
+        const { prefix } = require('./config.json');
+
+        message.channel.send('type help + {command name for specific help on that command}');
+        const command = client.commands.get(args[0]) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(args[0]));
+
+        // Check if command exists or if user inputted a command
+        if (!command || args[0] == undefined) {
+            var helpEmbed = new Discord.MessageEmbed()
+                .setColor('#FF69B4')
+                .setTitle(`All commands, To get help for each command do ${prefix}help {command name}`);
+            var fieldsToAdd = [];
+
+            // Finds all the categories of the commands
+            for (let commandName of client.commands.keys()) {
+                let categoryExists = false;
+                let commandCategory = client.commands.get(commandName).category;
+                for (var i = 0; i < fieldsToAdd.length; i++) {
+                    if (commandCategory == fieldsToAdd[i][0]) {
+                        fieldsToAdd[i][1].push(commandName);
+                        categoryExists = true;
+                    }
+                }
+                if (!categoryExists) {
+                    fieldsToAdd.push([commandCategory, [commandName]]);
+                }
+            }
+            let numCategory = fieldsToAdd.length;
+
+            // Adds category fields to embed
+            for (var i = 0; i < numCategory; i++) {
+                let fieldValue = "";
+                let numCommandsInCategory = fieldsToAdd[i][1].length;
+
+                // Adds commands to its respective category
+                for (var j = 0; j < numCommandsInCategory; j++) {
+                    fieldValue += "`" + fieldsToAdd[i][1][j] + "` ";
+                }
+                helpEmbed.addField(fieldsToAdd[i][0], fieldValue);
+            }
+            message.channel.send(helpEmbed);
+        }
+        else {
+            // Send embed of command's info
+            helpEmbed = new Discord.MessageEmbed()
+                .setColor('#FF69B4')
+                .setTitle(`${command.name.charAt(0).toUpperCase() + command.name.slice(1)} help`)
+                .addFields(
+                    { name: command.description, value: `Syntax: ${prefix}${command.name} ${command.syntax}` },
+                );
+            message.channel.send(helpEmbed);
+        }
+    }
+};
---
+++ commands/ping.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'ping',
+    description: 'Returns bot and API latency in milliseconds.',
+    syntax: "",
+    category: "Utility",
+    async execute(message, args) {
+        const client = require('../index.js').client;
+        const { MessageEmbed } = require('discord.js');
+
+        const msg = await message.channel.send('🏓 Pinging...');
+
+        const embed = new MessageEmbed()
+            .setColor('#000000')
+            .setTitle('🏓 Pong!')
+            .setDescription(`Bot Latency is **${Math.floor(msg.createdTimestamp - message.createdTimestamp)} ms** \nAPI Latency is **${Math.round(client.ws.ping)} ms**`);
+
+        message.channel.send(embed);
+    }
+}
